af60864de3a74d4e5ad14d71f2fba92a9fec6694
Saroj Mali <55340491+Saroj7499@users.noreply.github.com>
2019-09-29 09:36:28 -0700

    image reading using CV

---
+++ Demo_Img_reading.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as py
+import matplotlib.pyplot as pt
+
+#img = cv2.imread("saroj.jpg",cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread("saroj.jpg")
+#img2 = cv2.imread("saroj.jpg",cv2.IMREAD_GRAYSCALE)
+#cv2.imshow('image',img)
+cv2.imshow('image',img1)
+#cv2.imshow('image',img2)
+cv2.waitKey(0)
